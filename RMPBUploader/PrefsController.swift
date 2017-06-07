f40c85e38d8f2ccc379785652708c97c28955e42
Todd Williams <williatf@gmail.com>
2017-06-07 17:09:59 -0500

    added PrefsController with xib file

---
+++ RMPBUploader/PrefsController.swift
@@ -0,0 +1,19 @@
+//
+//  PrefsController.swift
+//  
+//
+//  Created by Todd Williams on 6/7/17.
+//
+//
+
+import Cocoa
+
+class PrefsController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
